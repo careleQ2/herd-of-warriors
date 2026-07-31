481824ac69e6b396ab01dee9a86c04df43ee3e39
CarlosArH <carlos.arranzh@edu.uah.es>
2026-07-31 16:12:17 +0200

    cambiar título línea 17

--- herd-of-warriors-sin-memes/src/routes/auth.tsx
+++ herd-of-warriors-sin-memes/src/routes/auth.tsx
@@ -14,7 +14,7 @@ export const Route = createFileRoute("/auth")({
   validateSearch: searchSchema,
   head: () => ({
     meta: [
-      { title: "Acceso — Herd of Warriors" },
+      { title: "Acceso — Herd of Warriors1" },
       { name: "description", content: "Crea tu cuenta o inicia sesión." },
     ],
   }),
